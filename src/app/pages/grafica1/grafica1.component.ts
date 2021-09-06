f6478de7a4e8c9af125f7b0da241635d5fbf3bd5
Valdi06 <david_061195@hotmail.com>
2021-09-05 20:27:35 -0500

    Grafica agregada

--- src/app/pages/grafica1/grafica1.component.ts
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { MultiDataSet, Label, Color } from 'ng2-charts';
+
 
 @Component({
   selector: 'app-grafica1',
@@ -6,11 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class Grafica1Component implements OnInit {
+export class Grafica1Component {
 
-  constructor() { }
+  public doughnutChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public doughnutChartData: MultiDataSet = [
+    [350, 450, 100], //conjunto datos 1
+  ];
 
-  ngOnInit(): void {
-  }
+  // Para cambiar los colores, se agrega una linea por cada conjunto de datos
+  public colors: Color[] = [
+    { backgroundColor: ['#6857E6','#009FEE','#F02059'] } //linea colores datos 1
+  ];
 
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
+// import { ChartsModule } from 'ng2-charts';
+import { ChartsModule } from 'ng2-charts';
+
+// Modulos
 import { SharedModule } from '../shared/shared.module';
+import { ComponentsModule } from '../components/components.module';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
-
 
 
 @NgModule({
@@ -26,8 +31,11 @@ import { PagesComponent } from './pages.component';
   ],
   imports: [
     CommonModule,
+    FormsModule,
+    ChartsModule,
     SharedModule,
-    RouterModule
+    RouterModule,
+    ComponentsModule
   ]
 })
 export class PagesModule { }
